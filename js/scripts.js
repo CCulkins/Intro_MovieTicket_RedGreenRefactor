a2442a73818609ecb8361f8d7c2e18b87ef06a1f
Cassandra Culkins and Mao Thygesen <maothygesen@gmail.com>
2016-01-26 14:56:52 -0800

    add ticketPrice method if else

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,27 @@ this.time = time;
 this.movie = movie;
 this.age = age;
 }
+
+Ticket.prototype.fullInfo = function () {
+  return this.time + ", " + this.movie + ", " + this.age;
+};
+
+Ticket.prototype.ticketPrice = function () {
+  if(this.time === "day" && this.movie === "old" && this.age === "senior"){
+    return 5;
+  }else if(this.time === "day" && this.movie === "old" && this.age === "reg"){
+    return 6;
+  }else if(this.time === "day" && this.movie=== "new" && this.age ==="senior"){
+    return 7;
+  }else if(this.time === "day" && this.movie ===  "new" && this.age === "reg"){
+    return 8;
+  }else if(this.time === "evening" && this.movie === "old" && this.age === "senior"){
+    return 10;
+  }else if(this.time === "evening" && this.movie === "old" && this.age === "reg"){
+    return 11;
+  }else if(this.time === "evening" && this.movie === "new" && this.age === "senior"){
+    return 12;
+  }else{
+    return 13;
+  }
+};
--- specs/specs.js
+++ specs/specs.js
@@ -1,8 +1,16 @@
 describe("Ticket",function(){
-  it("will create a constructor with these specifications", function(){
-    var newTicket = new Ticket("evening", "new", "reg");
-    expect(newTicket.time).to.equal("evening");
-    expect(newTicket.movie).to.equal("new");
-    expect(newTicket.age).to.equal("reg");
+  it("will create a Ticket with these specifications", function(){
+    var testTicket = new Ticket("evening", "new", "reg");
+    expect(testTicket.time).to.equal("evening");
+    expect(testTicket.movie).to.equal("new");
+    expect(testTicket.age).to.equal("reg");
+  });
+  it("return formatted string with the 3 variants", function() {
+    var testTicket = new Ticket("day", "old", "senior");
+    expect(testTicket.fullInfo()).to.equal("day, old, senior");
+  });
+  it("return the price of the ticket", function(){
+    var testTicket = new Ticket("day", "old", "senior");
+    expect(testTicket.ticketPrice()).to.equal(5);
   });
 });
